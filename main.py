7f44cb248c61812e173be0d4a07cef47ee278b26
Viktor Holta <viktor.holta@outlook.com>
2021-01-22 20:05:16 +0100

    NEW: init bot with test command

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+
+global cmd_prefix
+cmd_prefix = "!"
+
+class BotClient(discord.Client):
+    async def on_ready(self):
+        print("Logged in as", self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content[0] == cmd_prefix:
+            message_split = message.content[1:].split(" ")
+            command = message_split[0]
+            parameters = message_split[1:]
+            await self.on_command(message.author, message.channel, command, parameters)
+
+    async def on_command(self, user, channel, command, parameters):
+        if command == "staben" and parameters and parameters[0] == "quote":
+            await channel.send("Staben kan testa discord bot!")
+            
+
+client = BotClient()
+client.run("ODAyMjQzNzA1NzMzMzgyMTU1.YAsZrA.EiHf5HglwCRB1d1ups7VpZCvizQ")
