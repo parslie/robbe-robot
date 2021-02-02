a70c60eb9cfafb920db2048f8231bffbd80e74ca
Viktor Holta <viktor.holta@outlook.com>
2021-02-02 13:36:27 +0100

    NEW: add dice & react command to plans

--- texts.py
+++ texts.py
@@ -5,4 +5,6 @@ plans = """- A poll command, so that users can vote between up to 9 different th
         - A video command, so that users can show videos in a voice channel.
         - A music command, so that users can play music in a voice channel.
         - A source code command, that links to the repository where Robbe Robot lives.
-        - A STABEN command, so that the power of STABEN can be invoked... via quotes."""
+        - A STABEN command, so that the power of STABEN can be invoked... via quotes.
+        - A dice command, so that users can get a random number.
+        - A react command, so that users can react in certain ways via custom reaction images."""
