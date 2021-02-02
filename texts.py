23b1c2bd7879f17720c86473ab3485f8bb30c67e
Viktor Holta <viktor.holta@outlook.com>
2021-02-02 13:38:24 +0100

    NEw: add yet another plan

--- texts.py
+++ texts.py
@@ -7,4 +7,5 @@ plans = """- A poll command, so that users can vote between up to 9 different th
         - A source code command, that links to the repository where Robbe Robot lives.
         - A STABEN command, so that the power of STABEN can be invoked... via quotes.
         - A dice command, so that users can get a random number.
-        - A react command, so that users can react in certain ways via custom reaction images."""
+        - A react command, so that users can react in certain ways via custom reaction images.
+        - A counter command, so that users can create counters with specific titles. (e.g. Andröv Death counter)"""
