644a5f3b28a46756dda5dd2f5c0c43013efc0bd9
Viktor Holta <viktor.holta@outlook.com>
2021-02-24 22:53:27 +0100

    NEW: redesign all but counter command

--- src/commands.py
+++ src/commands.py
@@ -16,8 +16,8 @@ class Command():
     def details(self):
         return self.description
 
-    async def send_message(self, channel, title=None, description=None):
-        embed = discord.Embed(title=title, description=description)
+    async def send_message(self, channel, title=None, description=None, url=None):
+        embed = discord.Embed(title=title, description=description, url=url)
         await channel.send(embed=embed)
 
     async def execute(self, user, channel, arguments):
@@ -55,7 +55,7 @@ class Erik(Command):
 
 class Donken(Command):
     def __init__(self):
-       super().__init__("donken", "asdasdsdfdsafler?")
+       super().__init__("donken", "Reads your soul and decides your next donken meal.")
     
     async def execute(self, user, channel, arguments):
         await super().execute(user, channel, arguments)
@@ -68,13 +68,13 @@ class Donken(Command):
 
 class Dice(Command):
     def __init__(self):
-       super().__init__("dice", "Asdfasdf att snacka eller?")
+       super().__init__("dice", "Generates a random number from 1 to 6 or higher.")
 
     def usage(self):
-        return "{} **{}**".format(self.name, "[MAX]")
+        return "{} {}".format(self.name, "[MAX]")
 
     def details(self):
-        return self.description + "\n\n[MAX] - The amount of sides the die has. Defaults to 6."
+        return self.description + "\n\n**[MAX]** - The amount of sides the die has. Defaults to 6."
 
     async def execute(self, user, channel, arguments):
         await super().execute(user, channel, arguments)
@@ -93,28 +93,74 @@ class Dice(Command):
 
 
 class Plans(Command):
-    pass
+    def __init__(self):
+        super().__init__("plans", "Displays the plans for Robbe Robot.")
+    
+    async def execute(self, user, channel, arguments):
+        await super().execute(user, channel, arguments)
+
+        if len(arguments) > 0: return
+
+        await self.send_message(channel, "Plans for Robbe Robot", \
+            """- A **poll** command, so that users can vote between up to 9 different things.
+            - A **video** command, so that users can show videos in a voice channel.
+            - A **music** command, so that users can play music in a voice channel.
+            - A **react** command, so that users can react in certain ways via custom reaction images.""")
 
 
 class Help(Command):
-    pass
+    def __init__(self):
+        super().__init__("help", "Displays all available commands for Robbe Robot.")
+
+    async def show_all(self, channel):
+        text = ""
+
+        for cmd in cmds.values():
+            text += "**{}** - {}\n".format(cmd.name, cmd.description)
+
+        await self.send_message(channel, "Robbe Robot commands", text)
+
+    async def show_one(self, channel, cmd_name):
+        cmd = cmds.get(cmd_name)
+        if cmd != None:
+            await self.send_message(channel, cmd.usage(), cmd.details())
+
+    async def execute(self, user, channel, arguments):
+        await super().execute(user, channel, arguments)
+        
+        if len(arguments) == 0:
+            await self.show_all(channel)
+        elif len(arguments) == 1:
+            await self.show_one(channel, arguments[0])
+        else:
+            return
 
 
 class Counter(Command):
-    pass
+    def __init__(self):
+        super().__init__("counter", "Manipulates and displays counter, such as the Andröv Death counter.")
 
 
 class Source(Command):
-    pass
+    def __init__(self):
+        super().__init__("source", "Gives the direction to Robbe Robot's residence.")
+
+    async def execute(self, user, channel, arguments):
+        await super().execute(user, channel, arguments)
+
+        if len(arguments) > 0: return
+
+        await self.send_message(channel, "Robbe Robot's residence", "This is where Robbe Robot lives and spends most of his time!", "https://github.com/Parslie/robbe-robot")
 
 
 # Activate Commands
 
 
 Staben()
-#Help()
+Help()
 #Counter()
 Dice()
 Donken()
 Erik()
-#Source()
+Plans()
+Source()
