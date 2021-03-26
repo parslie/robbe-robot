468b838cd480fe5a0f308099e9967de4382606e3
Viktor Holta <viktor.holta@outlook.com>
2021-03-26 12:05:47 +0100

    NEW: define save() & init()

--- src/quote.py
+++ src/quote.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 custom_fn = "custom_quotes.json"
 default_fn = "default_quotes.json"
@@ -7,8 +8,17 @@ default_quotes = dict()
 
 
 def save():
-    pass
+    with open(custom_fn, 'w') as f:
+        f.write(json.dumps(custom_quotes))
 
 
 def init():
-    pass
+    global default_quotes
+    global custom_quotes
+
+    if os.path.exists(default_fn):
+        with open(default_fn, 'r') as f:
+            default_quotes = json.loads(f.read())
+    if os.path.exists(custom_fn):
+        with open(custom_fn, 'r') as f:
+            custom_quotes = json.loads(f.read())
