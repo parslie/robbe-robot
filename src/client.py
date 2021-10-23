a44ab771af78f276a0f4b8d88674d112e29469d8
Viktor Holta <viktor.holta@outlook.com>
2021-10-23 17:18:15 +0200

    Prepare command-processing

--- src/client.py
+++ src/client.py
@@ -1,5 +1,17 @@
 import discord
+from discord import colour
+
+PREFIX = '!'
 
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Signed in as {self.user}!')
+
+    async def on_message(self, msg: discord.Message):
+        if msg.author == self.user:
+            return  # Do not respond to self
+
+        if msg.content and msg.content[0] == PREFIX:
+            await msg.channel.send(content=f'{msg.author} sent "{msg.content}"')
+            embed = discord.Embed(title='Title', description='Description')
+            await msg.channel.send(embed=embed)
