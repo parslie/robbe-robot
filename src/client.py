4cb477cc582949f3a919119cf772ae63fce7756a
Viktor Holta <viktor.holta@outlook.com>
2021-10-23 19:16:24 +0200

    Finish command-processing & handle invalid argument syntax

--- src/client.py
+++ src/client.py
@@ -1,7 +1,54 @@
 import discord
-from discord import colour
+from discord import Colour
 
 PREFIX = '!'
+
+
+def process_cmd(cmd: str):
+    if ' ' not in cmd:
+        return cmd, []
+    name, arg_str = cmd.split(None, 1)
+
+    args = []
+    new_arg = ''
+    is_quote = False
+    prev_c = None
+
+    for c in arg_str:
+        if c == '"':
+            # Toggle quote mode
+            if not is_quote and prev_c != ' ' and prev_c != None:
+                # Handle starting quotes not being preceded by a space
+                return None, 'Non-space character followed up with a start quote!'
+            is_quote = not is_quote
+        
+        elif not is_quote and c != ' ' and prev_c == '"':  
+            # Handle ending quotes not followed by a space
+            return None, 'End quote followed up with a non-space character!'
+
+        elif not is_quote and c == ' ': 
+            # Add previous argument
+            if prev_c == ' ':
+                # Skip if previous was a space too
+                continue
+            args.append(new_arg)
+            new_arg = ''
+
+        else: 
+            # Add character to current argument
+            new_arg += c
+        
+        prev_c = c
+
+    # Handle unended quotes
+    if is_quote:
+        return None, 'Last quote not ended!'
+
+    # Add last argument
+    args.append(new_arg)
+
+    return name, args
+
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -12,6 +59,10 @@ class Client(discord.Client):
             return  # Do not respond to self
 
         if msg.content and msg.content[0] == PREFIX:
-            await msg.channel.send(content=f'{msg.author} sent "{msg.content}"')
-            embed = discord.Embed(title='Title', description='Description')
-            await msg.channel.send(embed=embed)
+            name, args = process_cmd(msg.content[1:])
+            
+            if name is None:
+                embed = discord.Embed(title=f'ERROR: {args}', colour=Colour.red())
+                await msg.channel.send(embed=embed)
+            else:
+                pass
