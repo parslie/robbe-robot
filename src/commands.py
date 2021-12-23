43ef6e35d244501cbd02e5c8516dadc495dd8d39
Viktor Holta <viktor.holta@outlook.com>
2021-12-23 09:49:29 +0100

    Finish counter command

--- src/commands.py
+++ src/commands.py
@@ -20,7 +20,7 @@ class Command:
         return 'N/A'
 
     async def execute(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        await util.send_error(channel, f'The command **{self.name}** has not been implemented yet')
+        await util.send_error(channel, f'The command __{self.name}__ has not been implemented yet')
 
 
 def bind_command(cls: Command):
@@ -39,12 +39,12 @@ class ModedCommand(Command):
 
     async def execute(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if not args:
-            await util.send_error(channel, f'You need to specify a mode for the command **{self.name}**')
+            await util.send_error(channel, f'You need to specify a mode for the command __{self.name}__')
         else:
             mode = args[0]
             
             if mode not in self.modes.keys():
-                await util.send_error(channel, f'**{mode}** is not a valid mode for the command **{self.name}**')
+                await util.send_error(channel, f'__{mode}__ is not a valid mode for the command __{self.name}__')
             else:
                 await self.modes[mode](client, channel, author, args[1:])
 
@@ -68,9 +68,9 @@ class Help(Command):
             cmd = cmds.get(cmd_name)
 
             if cmd is None:
-                await util.send_warning(channel, f'The command **{cmd_name}** does not exist!')
+                await util.send_warning(channel, f'The command __{cmd_name}__ does not exist')
             else:
-                await util.send_success(channel, f'Help for **{cmd_name}**', cmd.help())
+                await util.send_success(channel, f'Help for __{cmd_name}__', cmd.help())
         elif len(args) == 0:
             txt = ''
             for cmd in cmds.values():
@@ -79,7 +79,8 @@ class Help(Command):
             embed = Embed(title='Commands and Info', description=txt, colour=Colour.green())
             await channel.send(embed=embed)
         else:
-            await util.send_error(channel, f'The command **{self.name}** needs only 0 or 1 argument')
+            await util.send_error(channel, f'The command __{self.name}__ needs only 0 or 1 argument')
+
 
 @bind_command
 class Game(ModedCommand):
@@ -120,7 +121,7 @@ class Game(ModedCommand):
 
     async def add(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) < 2:
-            await util.send_error(channel, 'The mode **add** needs at least 2 arguments')
+            await util.send_error(channel, 'The mode __add__ needs at least 2 arguments')
 
         else:  # Add user to game list # TODO: add safety check to see if the users are actually users
             game = args[0]
@@ -135,14 +136,14 @@ class Game(ModedCommand):
                 self.games[game] = game_list
 
                 data.save(self.games, 'data/game.json')
-                await util.send_success(channel, f'Added users to **{game}**:', txt)
+                await util.send_success(channel, f'Added users to __{game}__:', txt)
 
             else:  # Warn about already included users
-                await util.send_warning(channel, f'All of the specified users were already in **{game}**!')
+                await util.send_warning(channel, f'All of the specified users were already in __{game}__')
 
     async def remove(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) < 2:
-            await util.send_error(channel, 'The mode **remove** needs at least 2 arguments')
+            await util.send_error(channel, 'The mode __remove__ needs at least 2 arguments')
 
         else:  # Remove user from game list
             game = args[0]
@@ -161,14 +162,14 @@ class Game(ModedCommand):
                     self.games.pop(game)
 
                 data.save(self.games, 'data/game.json')
-                await util.send_success(channel, f'Removed users from **{game}**:', txt)
+                await util.send_success(channel, f'Removed users from __{game}__:', txt)
 
             else:  # Warn about already excluded users
-                await util.send_warning(channel, f'None of the specified users were in **{game}**!')
+                await util.send_warning(channel, f'None of the specified users were in __{game}__')
 
     async def call(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) != 1:
-            await util.send_error(channel, 'The mode **call** needs exactly 1 arguments')
+            await util.send_error(channel, 'The mode __call__ needs exactly 1 argument')
         
         else:  # Call for all epic gamers to game
             game = args[0]
@@ -181,11 +182,11 @@ class Game(ModedCommand):
                 await channel.send(content=txt)
 
             else:
-                await util.send_warning(channel, f'No users have been added to **{game}**!')
+                await util.send_warning(channel, f'No users have been added to __{game}__')
 
     async def list(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) != 0:
-            await util.send_error(channel, 'The mode **list** needs exactly 0 arguments')
+            await util.send_error(channel, 'The mode __list__ needs exactly 0 arguments')
 
         elif not self.games:  # Print warning for empty games list
             await util.send_warning(channel, 'There are no games to list!')
@@ -198,6 +199,7 @@ class Game(ModedCommand):
                     txt += f'{user} '
                 txt += '\n'
             await util.send_success(channel, 'Game Lists', txt)
+
 
 @bind_command
 class Quote(ModedCommand):
@@ -241,7 +243,7 @@ class Quote(ModedCommand):
 
     async def add(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) < 2:
-            await util.send_error(channel, 'The mode **add** needs at least 2 arguments')
+            await util.send_error(channel, 'The mode __add__ needs at least 2 arguments')
 
         # Add all specified quotes
         else:
@@ -253,12 +255,12 @@ class Quote(ModedCommand):
                 quotes.append(new_quote)
             self.quotes[quote_type] = quotes
 
-            await util.send_success(channel, f'Added **{len(new_quotes)}** new quotes of type **{quote_type}**')
+            await util.send_success(channel, f'Added __{len(new_quotes)}__ new quote(s) of type __{quote_type}__')
             data.save(self.quotes, 'data/quotes.json')
 
     async def remove(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) < 2:
-            await util.send_error(channel, 'The mode **remove** needs at least 2 arguments')
+            await util.send_error(channel, 'The mode __remove__ needs at least 2 arguments')
 
         # Remove all specified quotes
         else:
@@ -282,17 +284,17 @@ class Quote(ModedCommand):
                     quotes.pop(quote_index)
                 self.quotes[quote_type] = quotes
 
-                await util.send_success(channel, f'Removed **{len(quote_indices)}** quote(s) of type **{quote_type}**')
+                await util.send_success(channel, f'Removed __{len(quote_indices)}__ quote(s) of type __{quote_type}__')
                 data.save(self.quotes, 'data/quotes.json')
 
             # Warn against invalid indices
             else:
                 quotes = self.quotes.get(quote_type, [])
-                await util.send_warning(channel, f'There are only **{len(quotes)}** quote(s) of type **{quote_type}**', 'You might have entered and out-of-range or non-integer index.')
+                await util.send_warning(channel, f'There are only __{len(quotes)}__ quote(s) of type __{quote_type}__', 'You might have entered and out-of-range or non-integer index.')
 
     async def print(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) != 1:
-            await util.send_error(channel, 'The mode **print** needs exactly 1 argument')
+            await util.send_error(channel, 'The mode __print__ needs exactly 1 argument')
 
         # Print a random quote of a specific type
         else:
@@ -301,7 +303,7 @@ class Quote(ModedCommand):
 
             # Warn against empty quote type
             if not quotes:
-                await util.send_warning(channel, f'There are no quotes of type **{quote_type}**')
+                await util.send_warning(channel, f'There are no quotes of type __{quote_type}__')
 
             # Get and print a random quote
             else:
@@ -310,7 +312,7 @@ class Quote(ModedCommand):
 
     async def list(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if len(args) > 1:
-            await util.send_error(channel, 'The mode **list** needs 0 or 1 argument')
+            await util.send_error(channel, 'The mode __list__ needs 0 or 1 argument')
 
         # List all quotes of a specific type
         elif args:
@@ -319,14 +321,14 @@ class Quote(ModedCommand):
 
             # Warn against empty quote type
             if not quotes:
-                await util.send_warning(channel, f'There are no quotes of type **{quote_type}**')
+                await util.send_warning(channel, f'There are no quotes of type __{quote_type}__')
 
             # Print all quotes of type
             else:
                 txt = ''
                 for i, quote in enumerate(quotes):
                     txt += f'**{i}:** "{quote}"\n'
-                await util.send_success(channel, f'**{quote_type}** Quotes', txt)
+                await util.send_success(channel, f'__{quote_type}__ Quotes', txt)
 
 
         # List all quote types
@@ -347,6 +349,7 @@ class Quote(ModedCommand):
             # Print non-empty quote types
             else:
                 await util.send_success(channel, 'Quote types', txt)
+
 
 @bind_command
 class Donken(ModedCommand):
@@ -372,7 +375,7 @@ class Donken(ModedCommand):
                 Usage: **donken [mode]**
                 Modes: **meal** and **dessert**
 
-                The modes are self-explanatory.
+                The modes are self-explanatory. Do you want a **meal** or a **dessert**?
                '''
 
     def get_seed(self, user_id):
@@ -380,9 +383,10 @@ class Donken(ModedCommand):
         quarter = math.floor(now.minute / 15)
         return f'{now.month}{now.day}{now.hour}{quarter}{user_id}'
 
+    # TODO: change messages
     async def meal(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if args:
-            await util.send_error(channel, f'The mode **meal** needs no arguments')
+            await util.send_error(channel, f'The mode __meal__ needs no arguments')
         else:
             random.seed(self.get_seed(author.id))
             meal = random.choice(self.meals)
@@ -392,7 +396,7 @@ class Donken(ModedCommand):
 
     async def dessert(self, client: Client, channel: TextChannel, author: User, args: List[str]):
         if args:
-            await util.send_error(channel, f'The mode **dessert** needs no arguments')
+            await util.send_error(channel, f'The mode __dessert__ needs no arguments')
         else:
             random.seed(self.get_seed(author.id))
             dessert = random.choice(self.desserts)
@@ -402,24 +406,156 @@ class Donken(ModedCommand):
 @bind_command
 class Counter(ModedCommand):
     def __init__(self):
-        super().__init__('counter', 'N/A')
+        super().__init__('counter', 'keeps track of number counters')
         self.bind_mode('create', self.create)
-        self.bind_mode('remove', self.remove)
-        self.bind_mode('list', self.list)
         self.bind_mode('increment', self.increment)
         self.bind_mode('decrement', self.decrement)
+        self.bind_mode('list', self.list)
+
+        self.counters = data.load('data/counters.json')
+        
+    def help(self):
+        return '''
+                Keeps track of number counters, like the *Andröv Phasmophobia Death Counter*.
+                Usage: **counter [mode]**
+                Modes: **create**, **increment**, **decrement** and **list**
+
+                **Create mode:**
+                Creates a new counter with the specified ID and name.
+                Usage: **counter create [ID] [name]**
+                Example: **counter create anddeaths "Andröv Phasmophobia Deaths"**
+
+                **Increment mode:**
+                Increments a counter with the specified amount. The amount is 1 if left blank.
+                Usage: **counter increment [ID] [amount]**
+                Example: **counter increment anddeaths**
+                Example: **counter increment anddeaths 69420**
+                
+                **Decrement mode:**
+                Decrements a counter with the specified amount. The amount is -1 if left blank.
+                Usage: **counter decrement [ID] [amount]**
+                Example: **counter decrement anddeaths**
+                Example: **counter decrement anddeaths 31415**
+                
+                **List mode:**
+                Lists all existing counters or a specific counter.
+                Usage: **counter list [ID]**
+                Example: **counter list**
+                Example: **counter list anddeaths**
+               '''
 
     async def create(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        pass
+        if len(args) != 2:
+            await util.send_error(channel, 'The mode __create__ needs exactly 2 arguments')
 
-    async def remove(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        pass
+        # Create a counter with a specific name
+        else:
+            counter_id = args[0]
+            counter_name = args[1]
 
-    async def list(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        pass
+            if counter_id in self.counters.keys():
+                await util.send_warning(channel, f'A counter with the ID __{counter_id}__ already exists')
+            else:
+                self.counters[counter_id] = [counter_name, 0]
+
+                data.save(self.counters, 'data/counters.json')
+                await util.send_success(channel, f'Created the counter __{counter_name}__ with the ID __{counter_id}__')
 
     async def increment(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        pass
+        if not args or len(args) > 2:
+            await util.send_error(channel, 'The mode __increments__ needs 1 or 2 arguments')
+
+        # Increment by one
+        elif len(args) == 1:
+            counter_id = args[0]
+
+            if counter_id not in self.counters.keys():
+                await util.send_warning(channel, f'The counter __{counter_id}__ does not exist')
+            else:
+                counter = self.counters[counter_id]
+                counter[1] += 1
+                self.counters[counter_id] = counter
+
+                data.save(self.counters, 'data/counters.json')
+                await util.send_success(channel, f'Incremented the counter __{counter[0]}__ from __{counter[1] - 1}__ to __{counter[1]}__')
+
+        # Increment by a specific amount
+        else:
+            counter_id = args[0]
+            amount = args[1]
+
+            if not amount.isnumeric():
+                await util.send_error(channel, 'The second argument needs to be a non-negative integer')
+            elif counter_id not in self.counters.keys():
+                await util.send_warning(channel, f'The counter __{counter_id}__ does not exist')
+            else:
+                amount = int(amount)
+
+                counter = self.counters[counter_id]
+                counter[1] += amount
+                self.counters[counter_id] = counter
+
+                data.save(self.counters, 'data/counters.json')
+                await util.send_success(channel, f'Incremented the counter __{counter[0]}__ from __{counter[1] - amount}__ to __{counter[1]}__')
 
     async def decrement(self, client: Client, channel: TextChannel, author: User, args: List[str]):
-        pass
+        if not args or len(args) > 2:
+            await util.send_error(channel, 'The mode __decrement__ needs 1 or 2 arguments')
+
+        # Decrement by one
+        elif len(args) == 1:
+            counter_id = args[0]
+
+            if counter_id not in self.counters.keys():
+                await util.send_warning(channel, f'The counter __{counter_id}__ does not exist')
+            else:
+                counter = self.counters[counter_id]
+                counter[1] -= 1
+                self.counters[counter_id] = counter
+
+                data.save(self.counters, 'data/counters.json')
+                await util.send_success(channel, f'Incremented the counter __{counter[0]}__ from __{counter[1] + 1}__ to __{counter[1]}__')
+
+        # Decrement by a specific amount
+        else:
+            counter_id = args[0]
+            amount = args[1]
+
+            if not amount.isnumeric():
+                await util.send_error(channel, 'The second argument needs to be a non-negative integer')
+            elif counter_id not in self.counters.keys():
+                await util.send_warning(channel, f'The counter __{counter_id}__ does not exist')
+            else:
+                amount = int(amount)
+
+                counter = self.counters[counter_id]
+                counter[1] -= amount
+                self.counters[counter_id] = counter
+
+                data.save(self.counters, 'data/counters.json')
+                await util.send_success(channel, f'Decremented the counter __{counter[0]}__ from __{counter[1] + amount}__ to __{counter[1]}__')
+
+    async def list(self, client: Client, channel: TextChannel, author: User, args: List[str]):
+        if len(args) > 1:
+            await util.send_error(channel, 'The mode __list__ needs 0 or 1 argument')
+
+        # List a specific counter
+        elif len(args) == 1:
+            counter_id = args[0]
+            
+            if counter_id not in self.counters.keys():
+                await util.send_warning(channel, f'The counter __{counter_id}__ does not exist')
+            else:
+                counter = self.counters[counter_id]
+                await util.send_success(channel, f'The counter __{counter[0]}__ is at __{counter[1]}__')
+
+        # List all counters
+        else:
+            text = ''
+
+            for counter_id in self.counters.keys():
+                counter_name, counter_value = self.counters[counter_id]
+                text += f'**{counter_id} (**{counter_name}**):** {counter_value}\n'
+
+            await util.send_success(channel, 'All current counters', text)
+
